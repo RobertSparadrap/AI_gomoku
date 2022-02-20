941a3c3f277c205be5434a8145baede150ac6467
robert <fddd@>
2022-02-20 11:24:28 +0100

    exe

--- compile.py
+++ compile.py
@@ -1,5 +1,29 @@
-import os
+from os import system
 
-os.system("pip install pyinstaller")
-os.system("pyinstaller pbrain_gomoku_main.py --name pbrain-gomoku.exe --onefile")
-os.system('copy .\\dist\\pbrain-gomoku.exe .')
+
+files =  [
+    "src\\About.py",
+    "src\\Begin.py",
+    "src\\Board.py",
+    "src\\Check.py",
+    "src\\Direction.py",
+    "src\\EndProg.py",
+    "src\\EvalCoord.py",
+    "src\\EvalWeights.py",
+    "src\\GameBoard.py",
+    "src\\IA.py",
+    "src\\Info.py",
+    "src\\main.py",
+    "src\\Node.py",
+    "src\\Start.py",
+    "src\\Turn.py",
+    "src\\Win.py",
+]
+
+files_list = ""
+for file in files:
+    files_list += " " + file
+
+system("pip install pyinstaller")
+system("pyinstaller" + files_list + " --name pbrain-gomoku-ai.exe --onefile")
+system('copy .\\dist\\pbrain-gomoku-ai.exe .')
